cd49cb3f6a5df3926772e3c2496474bc1fa624c5
风来 <heii@qq.com>
2024-10-06 12:34:54 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import AppTable from './components/AppTable.vue'
+</script>
+
+<template>
+  <div class="container py-4 px-3 mx-auto">
+    <AppTable />
+  </div>
+</template>
---
+++ src/components/AppTable.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { BTable } from 'bootstrap-vue-next'
+
+const sortItems = [
+  { isActive: true, age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
+  { isActive: true, age: 45, first_name: 'Zelda', last_name: 'Macdonald' },
+  { isActive: false, age: 21, first_name: 'Larsen', last_name: 'Shaw' },
+  { isActive: false, age: 89, first_name: 'Geneva', last_name: 'Wilson' },
+  { isActive: false, age: 89, first_name: 'Gary', last_name: 'Wilson' },
+  { isActive: true, age: 38, first_name: 'Jami', last_name: 'Carney' },
+]
+
+const sortFields = [
+  { key: 'last_name', sortable: true },
+  { key: 'first_name', sortable: true },
+  { key: 'age', sortable: true },
+  { key: 'isActive', sortable: false },
+]
+</script>
+
+<template>
+  <BTable :sort-by="[{ key: 'first_name', order: 'desc' }]" :items="sortItems" :fields="sortFields" />
+</template>

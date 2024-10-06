fa2b1cca2dfede23a3d49e07e9ed9931384bd1fb
风来 <heii@qq.com>
2024-10-07 01:04:11 +0800

    各种折腾，真心觉得这套UI很弱

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,5 @@ import AppTable from './components/AppTable.vue'
 </script>
 
 <template>
-  <div class="container py-4 px-3 mx-auto">
-    <AppTable />
-  </div>
+  <AppTable />
 </template>
--- src/components/AppTable.vue
+++ src/components/AppTable.vue
@@ -1,7 +1,8 @@
 <script setup>
+import { ref } from 'vue'
 import { BTable } from 'bootstrap-vue-next'
 
-const sortItems = [
+const tableItems = [
   { isActive: true, age: 23, gender: 1, fullName: '张三' },
   { isActive: true, age: 34, gender: 0, fullName: '李四' },
   { isActive: false, age: 45, gender: 1, fullName: '王五' },
@@ -10,24 +11,41 @@ const sortItems = [
   { isActive: true, age: 18, gender: 1, fullName: '陈八' },
 ]
 
-const sortFields = [
+const tableFields = [
   { key: 'fullName', label: '姓名', sortable: true },
   { key: 'gender', label: '性别', sortable: true },
   { key: 'age', label: '年龄', sortable: true },
   { key: 'isActive', label: '是否激活', sortable: false },
 ]
+
+const totalRows = ref(tableItems.length)
+const currentPage = ref(1)
+const perPage = ref(4)
 </script>
 
 <template>
-  <BTable :sort-by="[{ key: 'fullName', order: 'asc' }]" :items="sortItems" :fields="sortFields">
-    <template #cell(gender)="row">
-      {{ row.value ? '男' : '女' }}
-    </template>
-    <template #cell(isActive)="row">
-      <div class="active" :class="row.value ? 'is-active' : 'no-active'">{{ row.value ? '已' : '未' }}激活</div>
-    </template>
-  </BTable>
+  <BContainer class="py-5">
+    <BTable :sort-by="[{ key: 'fullName', order: 'asc' }]" :items="tableItems" :fields="tableFields"
+      :current-page="currentPage" :per-page="perPage" hover bordered outlined>
+      <template #cell(gender)="row">
+        {{ row.value ? '男' : '女' }}
+      </template>
+      <template #cell(isActive)="row">
+        <div class="active" :class="row.value ? 'is-active' : 'no-active'">{{ row.value ? '已' : '未' }}激活</div>
+      </template>
+    </BTable>
+    <BRow>
+      <BCol>
+        <BPagination v-model="currentPage" :total-rows="totalRows" :per-page="perPage" class="my-0" />
+      </BCol>
+      <BCol class="current">
+        <strong>{{ totalRows }}</strong>条记录, 当前第 <strong>{{ currentPage }}</strong> 页, 共 <strong>{{ Math.ceil(totalRows
+          /
+          perPage) }}</strong> 页
+      </BCol>
+    </BRow>
 
+  </BContainer>
 </template>
 
 <style lang="scss" scoped>
@@ -40,6 +58,17 @@ const sortFields = [
 
   &.no-active {
     color: orange;
+  }
+}
+
+.current {
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+
+  strong {
+    color: darkgoldenrod;
+    margin: 0 5px;
   }
 }
 </style>

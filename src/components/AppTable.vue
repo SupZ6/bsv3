08f7796594109b4a8b9afa5f00dd475c2e599807
风来 <heii@qq.com>
2024-10-06 23:34:08 +0800

    更新table示例

--- src/components/AppTable.vue
+++ src/components/AppTable.vue
@@ -2,22 +2,44 @@
 import { BTable } from 'bootstrap-vue-next'
 
 const sortItems = [
-  { isActive: true, age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
-  { isActive: true, age: 45, first_name: 'Zelda', last_name: 'Macdonald' },
-  { isActive: false, age: 21, first_name: 'Larsen', last_name: 'Shaw' },
-  { isActive: false, age: 89, first_name: 'Geneva', last_name: 'Wilson' },
-  { isActive: false, age: 89, first_name: 'Gary', last_name: 'Wilson' },
-  { isActive: true, age: 38, first_name: 'Jami', last_name: 'Carney' },
+  { isActive: true, age: 23, gender: 1, fullName: '张三' },
+  { isActive: true, age: 34, gender: 0, fullName: '李四' },
+  { isActive: false, age: 45, gender: 1, fullName: '王五' },
+  { isActive: false, age: 36, gender: 1, fullName: '周六' },
+  { isActive: false, age: 27, gender: 0, fullName: '赵七' },
+  { isActive: true, age: 18, gender: 1, fullName: '陈八' },
 ]
 
 const sortFields = [
-  { key: 'last_name', sortable: true },
-  { key: 'first_name', sortable: true },
-  { key: 'age', sortable: true },
-  { key: 'isActive', sortable: false },
+  { key: 'fullName', label: '姓名', sortable: true },
+  { key: 'gender', label: '性别', sortable: true },
+  { key: 'age', label: '年龄', sortable: true },
+  { key: 'isActive', label: '是否激活', sortable: false },
 ]
 </script>
 
 <template>
-  <BTable :sort-by="[{ key: 'first_name', order: 'desc' }]" :items="sortItems" :fields="sortFields" />
+  <BTable :sort-by="[{ key: 'fullName', order: 'asc' }]" :items="sortItems" :fields="sortFields">
+    <template #cell(gender)="row">
+      {{ row.value ? '男' : '女' }}
+    </template>
+    <template #cell(isActive)="row">
+      <div class="active" :class="row.value ? 'is-active' : 'no-active'">{{ row.value ? '已' : '未' }}激活</div>
+    </template>
+  </BTable>
+
 </template>
+
+<style lang="scss" scoped>
+.active {
+  font-weight: bolder;
+
+  &.is-active {
+    color: green;
+  }
+
+  &.no-active {
+    color: orange;
+  }
+}
+</style>
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,24 @@
 import { defineConfig } from 'vite'
+import { URL, fileURLToPath } from 'node:url';
 import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite'
 import { BootstrapVueNextResolver } from 'bootstrap-vue-next'
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '~': fileURLToPath(new URL('./', import.meta.url)),
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler',
+        additionalData: `@use "@/styles/scss/global.scss" as *;`
+      }
+    }
+  },
   plugins: [
     vue(),
     Components({

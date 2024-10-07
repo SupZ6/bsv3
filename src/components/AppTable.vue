80bab4f3740dbf238e6bf48b224e21d4e19be23e
风来 <heii@qq.com>
2024-10-07 22:21:19 +0800

    format files

--- src/components/AppTable.vue
+++ src/components/AppTable.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import { ref } from 'vue'
+import { ref } from 'vue';
 
 const tableItems = [
   { isActive: true, age: 23, gender: 1, fullName: '张三' },
@@ -7,24 +7,32 @@ const tableItems = [
   { isActive: false, age: 45, gender: 1, fullName: '王五' },
   { isActive: false, age: 36, gender: 1, fullName: '周六' },
   { isActive: false, age: 27, gender: 0, fullName: '赵七' },
-  { isActive: true, age: 18, gender: 1, fullName: '陈八' },
-]
+  { isActive: true, age: 18, gender: 1, fullName: '陈八' }
+];
 
 const tableFields = [
   { key: 'fullName', label: '姓名', sortable: true },
   { key: 'gender', label: '性别', sortable: true },
   { key: 'age', label: '年龄', sortable: true },
-  { key: 'isActive', label: '是否激活', sortable: false },
-]
+  { key: 'isActive', label: '是否激活', sortable: false }
+];
 
-const totalRows = ref(tableItems.length)
-const currentPage = ref(1)
-const perPage = ref(4)
+const totalRows = ref(tableItems.length);
+const currentPage = ref(1);
+const perPage = ref(4);
 </script>
 
 <template>
-  <BTable :sort-by="[{ key: 'fullName', order: 'asc' }]" :items="tableItems" :fields="tableFields"
-    :current-page="currentPage" :per-page="perPage" hover bordered outlined>
+  <BTable
+    :sort-by="[{ key: 'fullName', order: 'asc' }]"
+    :items="tableItems"
+    :fields="tableFields"
+    :current-page="currentPage"
+    :per-page="perPage"
+    hover
+    bordered
+    outlined
+  >
     <template #cell(gender)="row">
       {{ row.value ? '男' : '女' }}
     </template>
@@ -33,12 +41,17 @@ const perPage = ref(4)
     </template>
   </BTable>
   <BRow>
-    <BCol>
-      <BPagination v-model="currentPage" :total-rows="totalRows" :per-page="perPage" class="my-0" />
-    </BCol>
     <BCol class="current">
-      <strong>{{ totalRows }}</strong>条记录, 当前第 <strong>{{ currentPage }}</strong> 页,
-      共 <strong>{{ Math.ceil(totalRows / perPage) }}</strong> 页
+      共
+      <strong>{{ totalRows }}</strong>
+      条记录, 当前第
+      <strong>{{ currentPage }}</strong>
+      页, 共
+      <strong>{{ Math.ceil(totalRows / perPage) }}</strong>
+      页
+    </BCol>
+    <BCol class="page">
+      <BPagination v-model="currentPage" :total-rows="totalRows" :per-page="perPage" class="my-0" />
     </BCol>
   </BRow>
 </template>
@@ -58,12 +71,16 @@ const perPage = ref(4)
 
 .current {
   display: flex;
-  justify-content: flex-end;
   align-items: center;
 
   strong {
     color: darkgoldenrod;
     margin: 0 5px;
   }
+}
+
+.page {
+  display: flex;
+  justify-content: flex-end;
 }
 </style>
